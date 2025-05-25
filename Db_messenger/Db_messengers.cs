0b41b77804f88782db5e5c54993f5ac87a8b9d6a
Maks <rubelmaksum2404@gmail.com>
2025-05-26 01:22:42 +0300

    new

--- Db_messenger/Db_messengers.cs
+++ Db_messenger/Db_messengers.cs
@@ -17,11 +17,11 @@ namespace Db_messenger
         {
 
             base.OnConfiguring(optionsBuilder);
-            optionsBuilder.UseSqlServer(@"Data Source=192.168.1.121\SQLEXPRESS,1433;
+            optionsBuilder.UseSqlServer(@"Data Source=192.168.1.121,1433;
                                                 Initial Catalog=Group_Messenger_DB;
                                                 User ID=group_user;
                                                 Password=NewStrongPasswordHere;
-                                                Encrypt=True;
+                                                Encrypt=False;
                                                 TrustServerCertificate=True;
                                                 Application Intent=ReadWrite;
                                                 Multi Subnet Failover=False;",
